993941d481283c45084218e830b4bd6f784a940f
Jacob Marthaller <marthaller.jr@gmail.com>
2023-08-27 12:29:37 -0700

    Interpolate string and print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, Jacob!");
+    // println!("Hello, Jacob!");
+
+    let jacob = {
+        let name = "Jacob";
+        let age = 28;
+        let languages = ["Rust", "Python", "JavaScript"];
+        (name, age, languages)
+    };
+
+    println!(
+        "Hello, World! from {}, who is {} years old and knows {} languages",
+        jacob.0,
+        jacob.1,
+        jacob.2.join(", ")
+    );
 }
